8a288a017f424ca67b452a16119741e9bceaaa3e
zeynepstayn35 <satir@campus.tu-berlin.de>
2022-12-10 17:18:03 +0100

    code that does nothing

--- ElectroPlatingLines/multi_robot.cpp
+++ ElectroPlatingLines/multi_robot.cpp
@@ -1,6 +1,6 @@
 #include "ElectroPlatingLines.h"
 
-/* Define a struct named "robot_arm_1" that represents a robot arm
+// Define a struct named "robot_arm_1" that represents a robot arm
 struct robot_arm_1
 {    // Define a member named "robot_1_movement_time" that is a vector of ints
     std::vector<int> robot_1_movement_time;
@@ -9,28 +9,16 @@ struct robot_arm_1
     std::vector<tuple<int,int>> robot_1_route;
 };
 
-// Create an instance of the "robot_arm_1" struct named "arm"
-robot_arm_1 arm;
-// Initialize the "robot_1_movement_time" member of "arm" with the specified vector of ints
-arm.robot_1_movement_time = { 2, 3, 1};
-arm.robot_1_transport_time = { 1, 2, 3};
-arm.robot_1_processing_time = { 5, 4, 3};
-arm.robot_1_route = {(0,2),(2,1),(1,3)};
-
 struct robot_arm_2
-{ 
+{
     std::vector<int> robot_2_movement_time;
     std::vector<int> robot_2_transport_time;
     std::vector<int> robot_2_processing_time;
     std::vector<tuple<int, int>> robot_2_route;
 };
+// Create an instance of the "robot_arm_1" struct named "arm"
 
-robot_arm_2 arm;
-arm.robot_2_movement_time = { 2, 3, 1, 5 };
-arm.robot_2_transport_time = { 1, 2, 3, 4 };
-arm.robot_2_processing_time = { 5, 4, 3, 2 };
-arm.robot_2_route = { (3,5),(5,4),(4,6) };
-*/
+
 
 
 // 18. November 2022 Freitag. EN Gebaeude //
@@ -58,3 +46,19 @@ arm.robot_2_route = { (3,5),(5,4),(4,6) };
 // another possibility>
 //   another file where you can aufruf these functions
 // shared files for things we share. w juan 
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    robot_arm_1 arm;
+    // Initialize the "robot_1_movement_time" member of "arm" with the specified vector of ints
+    arm.robot_1_movement_time = { 1,2,3 };
+    arm.robot_1_transport_time = { 4,5,6 };
+    arm.robot_1_processing_time = { 7,8,9 };
+    arm.robot_1_route = { {0,2},{2,1},{1,3} };
+    
+    robot_arm_2 arm2;
+    arm2.robot_2_movement_time = { 2, 3, 1, 5 };
+    arm2.robot_2_transport_time = { 1, 2, 3, 4 };
+    arm2.robot_2_processing_time = { 5, 4, 3, 2 };
+    arm2.robot_2_route = { {3,5},{5,4},{4,6} };
+    return 0;
+}
