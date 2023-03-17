900f017645dcdbb1179c7ac4640f0d1f32daf76c
Kreijstal <rainb@tfwno.gf>
2023-03-17 12:34:03 +0000

    Fix runner bug

--- init.sh
+++ init.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-pushd ..
-git clone --depth=1 https://github.com/Kreijstal/Hadamard_product_counters.git
-popd
+
+if [ -d "../Hadamard_product_counters" ]; then
+  if git -C ../Hadamard_product_counters rev-parse; then
+    echo "Directory ../Hadamard_product_counters already exists and is a Git repository."
+  else
+    echo "Directory ../Hadamard_product_counters already exists but is not a Git repository."
+    exit 1
+  fi
+else
+  git clone --depth=1 https://github.com/Kreijstal/Hadamard_product_counters.git ../Hadamard_product_counters
+fi
+
 make all
